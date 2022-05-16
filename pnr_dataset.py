fb957e15145d10fc9f37289d85fb89d9426a3d07
idejie <i@idejie.com>
2022-05-16 15:40:38 +0800

    init from TVQAPlus

---
+++ pnr_dataset.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+import sys
+import h5py
+import pickle
+import numpy as np
+import torch
+import json
+import copy
+from easydict import EasyDict as edict
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset
+from tqdm import tqdm
+
+class TVQADataset(Dataset):
+    def __init__(self, opt, mode="train"):
+        self.opt = opt
+        self.inference = mode == "test" 
+        self.info = []
+        self.init_info()
+        
+    def init_info(self):
+        with open(self.opt.train_pnr_info,'r')as f:
+            
+        
+    
